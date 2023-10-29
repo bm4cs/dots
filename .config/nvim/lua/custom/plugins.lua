9c877573d6bc02e124f4d5204e0c02339f17fe97
Ben Simmonds <ben@bencode.net>
2023-10-29 22:00:37 +1100

    nvc custom configs

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,18 @@
+local plugins = {
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer"
+      }
+    }
+  }
+}
+return plugins
